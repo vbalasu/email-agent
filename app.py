9a4b120e85366e0a173fa86eb6d57a04256ce610
Vijay Balasubramaniam <vbalasu@gmail.com>
2024-12-22 11:41:33 -0800

    Added get_most_recent_email()

--- app.py
+++ app.py
@@ -24,6 +24,40 @@ def process_s3_event(event):
     return send_email(sender='Cloudmatica Agent <agent@cloudmatica.com>', recipient=incoming['sender'], 
            subject='Re: ' + incoming['subject'], body=output)
 
+@app.route('/recent')
+def get_most_recent_email():
+    import boto3
+    from datetime import datetime
+
+    s3 = boto3.client('s3')
+    
+    # List objects in the agent/ prefix
+    response = s3.list_objects_v2(
+        Bucket='cloudmatica',
+        Prefix='agent/'
+    )
+
+    if 'Contents' not in response:
+        return {'error': 'No emails found'}
+
+    # Sort by last modified timestamp
+    objects = sorted(
+        response['Contents'],
+        key=lambda x: x['LastModified'],
+        reverse=True
+    )
+    
+    if not objects:
+        return {'error': 'No emails found'}
+
+    # Get the most recent object key
+    most_recent = objects[0]['Key']
+    
+    return {
+        'most_recent_email': f's3://cloudmatica/{most_recent}',
+        'timestamp': objects[0]['LastModified'].isoformat()
+    }
+
 @app.route('/')
 def index():
     return 'email-agent is running. Send an email to agent@cloudmatica.com'
